1767e9e15eb9d49b5f334bc5524ca35825d56bfc
rainndev <182114243+rainndev@users.noreply.github.com>
2025-02-24 17:09:04 +0800

    refactor Accordion component: update FAQ entries and improve layout structure

--- src/pages/Accordion.tsx
+++ src/pages/Accordion.tsx
@@ -3,25 +3,19 @@ import AccordionItem from "../component/AccordionItem";
 const data = [
   {
     title: "What is Frontend Mentor, and how will it help me?",
-    desc: `Frontend Mentor offers realistic coding challenges to help developers
-          improve their frontend coding skills with projects in HTML, CSS, and
-          JavaScript. It's suitable for all levels and ideal for portfolio
-          building.`,
+    desc: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
   },
   {
-    title: "How do I get help with a challenge?",
-    desc: `Challenges have a dedicated chat in the Frontend Mentor community. There
-          you can ask questions and get help from other developers.`,
+    title: "Is Frontend Mentor free?",
+    desc: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
   },
   {
-    title: "How do I get help with a challenge?",
-    desc: `Challenges have a dedicated chat in the Frontend Mentor community. There
-          you can ask questions and get help from other developers.`,
+    title: "Can I use Frontend Mentor projects in my portfolio?",
+    desc: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
   },
   {
-    title: "How do I get help with a challenge?",
-    desc: `Challenges have a dedicated chat in the Frontend Mentor community. There
-          you can ask questions and get help from other developers.`,
+    title: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    desc: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
   },
 ];
 
@@ -39,23 +33,27 @@ const Accordion = () => {
         </div>
 
         {/* accordion container */}
-        <div className="absolute max-w-4xl bg-white w-full top-1/2 flex flex-col p-10 rounded-2xl">
-          {/* accordion top */}
-          <div className="flex justify-start space-x-5">
-            <div className="w-16 h-16 flex justify-center items-center">
-              <img
-                src="/public/faq-accordion-kit/assets/images/icon-star.svg"
-                className="w-full h-full"
-              />
+        <div className="absolute w-full h-full flex justify-center items-center">
+          <div className="max-w-4xl bg-white w-full flex flex-col p-10 rounded-2xl">
+            {/* accordion top */}
+            <div className="flex justify-start space-x-5">
+              <div className="w-16 h-16 flex justify-center items-center">
+                <img
+                  src="/public/faq-accordion-kit/assets/images/icon-star.svg"
+                  className="w-full h-full"
+                />
+              </div>
+              <h1 className="text-6xl font-bold text-[hsl(292_42%_14%)]">
+                FAQs
+              </h1>
             </div>
-            <h1 className="text-6xl font-bold text-[hsl(292_42%_14%)]">FAQs</h1>
-          </div>
 
-          {/* accordion bottom */}
-          <div className="mt-10 space-y-5">
-            {data.map((data) => (
-              <AccordionItem {...data} />
-            ))}
+            {/* accordion bottom */}
+            <div className="mt-10 space-y-5">
+              {data.map((data) => (
+                <AccordionItem {...data} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
