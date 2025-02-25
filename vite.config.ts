71f16c59dcd977c1692a95f61357003fec9cda25
rainndev <182114243+rainndev@users.noreply.github.com>
2025-02-25 23:56:38 +0800

    fix: update script path in index.html and adjust build scripts in package.json for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: "/Front-end-mentor-react/",
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
     port: 5173,
