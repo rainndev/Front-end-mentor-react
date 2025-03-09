ca8debd926494a96855b74bda8e0a4e63cdfd608
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-09 14:26:20 +0800

    feat(blog-preview-card): enhance styling and layout of Blog Preview Card component

--- src/component/BlogPreviewCard.tsx
+++ src/component/BlogPreviewCard.tsx
@@ -2,7 +2,7 @@ const BlogPreviewCard = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[hsl(47_88%_63%)] font-[Figtree] ">
       {/* Container */}
-      <div className="bg-white p-4 rounded-2xl border-1 border-[hsl(0_0%_42%)] shadow-[7px_7px]  w-[375px] space-y-4 mx-5">
+      <div className="bg-white p-5 rounded-2xl border-1 border-[hsl(0_0%_42%)] shadow-[10px_10px]  w-[375px] space-y-4 mx-5">
         {/* Image */}
 
         <div className="w-full">
@@ -15,16 +15,16 @@ const BlogPreviewCard = () => {
 
         {/* Info */}
         <div className="space-y-4 w-full">
-          <button className="bg-[hsl(47_88%_63%)] px-3 py-1 rounded-md font-bold text-xs">
+          <button className="bg-[hsl(47_88%_63%)] px-3 py-1 rounded-md font-bold text-sm">
             Learning
           </button>
           <p className="font-medium  text-sm">Published 21 Dec 2023</p>
 
-          <h1 className="hover:text-[hsl(47_88%_63%)] font-bold text-xl">
+          <h1 className="hover:text-[hsl(47_88%_63%)] font-extrabold text-2xl">
             HTML & CSS foundations
           </h1>
 
-          <p className="text-gray-500 text-sm ">
+          <p className="text-gray-500  ">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
