50572c5a356c5a78123a20e5507474d6f3f1972e
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-10 01:52:17 +0800

    feat(results-summary): improve responsive design and adjust text sizes in Results Summary component

--- src/component/ResultsSummary.tsx
+++ src/component/ResultsSummary.tsx
@@ -2,25 +2,29 @@ import data from "../data/results-summary-data.json";
 
 const ResultsSummary = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center font-[Hanken_grotesk] text-white  bg-gray-100 drop-shadow-xl shadow-[hsl(252,100%,67%)]">
+    <div className="w-screen h-screen flex justify-center md:items-center font-[Hanken_grotesk] text-white  bg-gray-100 drop-shadow-xl shadow-[hsl(252,100%,67%)]">
       {/* container */}
-      <div className="w-full bg-white rounded-4xl max-w-3xl grid grid-cols-2">
+      <div className="w-full bg-white rounded-4xl max-w-3xl grid grid-cols-1 md:grid-cols-2">
         {/* LEFT GRID SUMMARY */}
-        <div className="relative bg-[hsl(252_100%_67%)]  w-full p-10 flex items-center flex-col space-y-5 rounded-4xl">
+        <div className="relative bg-[hsl(252_100%_67%)]  w-full p-10  flex items-center flex-col space-y-3 md:space-y-5 rounded-b-4xl  md:rounded-4xl ">
           {/* result text */}
-          <h1 className="text-2xl font-semibold text-[hsl(241,100%,89%)]">
+          <h1 className="text-[clamp(1rem,1.5vw,1.5rem)] font-semibold text-[hsl(221,100%,96%)]">
             Your Result
           </h1>
           {/* Circle with data */}
-          <div className="w-48 h-48  rounded-full bg-gradient-to-b from-[hsla(256,72%,46%,1)] to-[hsla(241,72%,46%,0)] flex flex-col justify-center items-center p-12 space-y-2 ">
-            <h1 className="text-6xl text-white font-bold z-10">76</h1>
-            <p className="text-[16px] text-[hsl(241,100%,89%)]  font-bold">
+          <div className="w-40 h-40 md:w-48 md:h-48  rounded-full bg-gradient-to-b from-[hsla(256,72%,46%,1)] to-[hsla(241,72%,46%,0)] flex flex-col justify-center items-center p-12">
+            <h1 className=" text-[clamp(3.5rem,2vw,3.75rem)] text-white font-bold z-10">
+              76
+            </h1>
+            <p className="text-[clamp(1rem,2vw,16px)] text-[hsl(241,100%,89%)]  font-bold">
               of 100
             </p>
           </div>
           {/* info */}
-          <h3 className="text-3xl font-semibold z-10">Great</h3>
-          <p className="text-center z-10 text-lg mx-4 text-[hsl(241,100%,89%)]">
+          <h3 className="text-[clamp(1.5rem,2vw,1.875rem)] font-semibold z-10">
+            Great
+          </h3>
+          <p className=" text-[clamp(1rem,2vw,1.125rem)] text-center z-10  mx-4 text-[hsl(241,100%,89%)]">
             You scored higher than 65% of the people who have taken these tests.
           </p>
 
@@ -33,7 +37,7 @@ const ResultsSummary = () => {
         </div>
 
         {/* Right grid - Summary */}
-        <div className="bg-white w-full rounded-4xl p-10 space-y-8">
+        <div className="bg-white w-full rounded-4xl p-[clamp(1.2rem,2vw,2.5rem)] space-y-8">
           {/* Title */}
           <h1 className="text-[hsl(224,30%,27%)] text-2xl font-bold">
             Summary
@@ -47,9 +51,9 @@ const ResultsSummary = () => {
                 key={i}
                 className="p-4 flex justify-between rounded-xl "
               >
-                <div className="flex space-x-3">
+                <div className="flex space-x-3 text-[clamp(1rem,2vw,1.125rem)]">
                   <img src={icon} alt={category} />
-                  <p className="font-semibold text-lg" style={{ color: clr }}>
+                  <p className="font-semibold" style={{ color: clr }}>
                     {category}
                   </p>
                 </div>
@@ -64,7 +68,7 @@ const ResultsSummary = () => {
           </div>
 
           {/* Button continue */}
-          <button className="text-center bg-[hsl(224,30%,27%)] w-full py-3 rounded-full text-lg">
+          <button className="text-center bg-[hsl(224,30%,27%)] w-full py-3 rounded-full  text-[clamp(1rem,2vw,1.125rem)]">
             Continue
           </button>
         </div>
