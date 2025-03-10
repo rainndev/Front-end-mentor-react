70965482dedaed751feff5b67fcfbef4f8da9d22
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-10 14:49:43 +0800

    feat(ticket-generator): update font and enhance layout with background image in Ticket Generator component

--- src/pages/TicketGeneratorMain.tsx
+++ src/pages/TicketGeneratorMain.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 const TicketGeneratorMain = () => {
-  return <div>TicketGeneratorMain</div>;
+  return (
+    <div className="text-white w-screen h-screen bg-[hsl(248,70%,10%)] bg-cover bg-no-repeat bg-center flex justify-center items-center bg-[url(/Front-end-mentor-react/conference-ticket-generator/assets/images/background-desktop.png)] font-[Inconsolata]">
+      {/* Coding conf */}
+      <div>
+        <img
+          src="/Front-end-mentor-react/conference-ticket-generator/assets/images/logo-full.svg"
+          alt="Full logo"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TicketGeneratorMain;
