321ee051329694a162516a97e70ca6bc6c55a9fd
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-13 01:00:22 +0800

    fix(ticket-generator): update image dimensions and adjust text scaling for better responsiveness

--- src/pages/TicketGeneratorMain.tsx
+++ src/pages/TicketGeneratorMain.tsx
@@ -64,13 +64,13 @@ const TicketGeneratorMain = () => {
         src="/Front-end-mentor-react/conference-ticket-generator/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg "
         alt=""
       />
-      <div className=" text-white w-screen min-h-screen bg-[hsl(248,70%,10%)] bg-cover bg-no-repeat bg-center flex flex-col  items-center bg-[url(/Front-end-mentor-react/conference-ticket-generator/assets/images/pattern-lines.svg),url(/Front-end-mentor-react/conference-ticket-generator/assets/images/background-desktop.png)] font-[Inconsolata] px-4 py-20 ">
+      <div className=" text-white w-screen min-h-screen bg-[hsl(248,70%,10%)] bg-cover bg-no-repeat bg-center flex flex-col  items-center bg-fixed bg-[url(/Front-end-mentor-react/conference-ticket-generator/assets/images/pattern-lines.svg),url(/Front-end-mentor-react/conference-ticket-generator/assets/images/background-desktop.png)] font-[Inconsolata] px-4 py-20 ">
         {/* Top Info */}
         <div className="flex flex-col justify-center items-center mx-2 lg:mx-36 mt-20">
           <img
             src="/Front-end-mentor-react/conference-ticket-generator/assets/images/logo-full.svg"
             alt="Full logo"
-            className=" w-32 mb-7 md:w-44"
+            className=" w-35 mb-7 md:w-44"
           />
 
           <h1 className="text-[clamp(1.5rem,5vw,3rem)] font-bold text-center text-pretty  mb-3">
@@ -78,7 +78,7 @@ const TicketGeneratorMain = () => {
             2025 Starts Here!
           </h1>
 
-          <p className="text-[hsl(252,6%,83%)] text-center text-[clamp(1rem,5vw,1.125rem)]">
+          <p className="text-[hsl(252,6%,83%)] text-center text-[clamp(1rem,2vw,1.125rem)]">
             Secure your spot at next year's biggest coding conference.
           </p>
         </div>
@@ -160,7 +160,7 @@ const TicketGeneratorMain = () => {
                 alt=""
               />
 
-              <p className="text-sm text-[hsl(245,15%,58%)]">
+              <p className=" text-[clamp(.8rem,3vw,.875rem)] text-[hsl(245,15%,58%)]">
                 Upload your photo (JPG or PNG, max size: 500KB).
               </p>
             </div>
