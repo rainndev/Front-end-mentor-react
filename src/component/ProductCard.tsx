f6eb002e5ec1e95a247fe054aa0cb566c487629c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-14 22:04:05 +0800

    feat(ProductList): restructure layout and add ProductCard component

---
+++ src/component/ProductCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductCard = () => {
+  return <div className="w-full h-55 bg-amber-100 rounded-lg">test</div>;
+};
+
+export default ProductCard;
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,14 +1,23 @@
 import ProductData from "../data/product-list-data.json";
+import ProductCard from "../component/ProductCard";
 
 const ProductList = () => {
   return (
-    <div className="text-white font-[Red_hat_text]">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        suscipit magnam commodi ad velit rerum non magni in assumenda illo
-        pariatur, mollitia hic exercitationem, officiis repellat, repellendus
-        cum alias quam.
-      </h1>
+    <div className="w-screen h-screen text-[#212121] font-[Red_hat_text] flex justify-center items-center  ">
+      <div className="grid grid-cols-1 md:grid-cols-[65%_35%] w-full  max-w-6xl ">
+        {/* Left container */}
+        <div className="bg-amber-300 w-full p-8">
+          <h1 className="text-2xl font-bold">Desserts</h1>
+          {/* product grid */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {[...Array(9)].map((_, i) => (
+              <ProductCard key={i} />
+            ))}
+          </div>
+        </div>
+        {/* Right container */}
+        <div className="bg-amber-500 w-full">right container</div>
+      </div>
     </div>
   );
 };
