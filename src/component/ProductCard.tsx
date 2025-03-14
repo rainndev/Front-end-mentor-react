7ad9d0f5e78b2a214c33e7ce5cdd0e3d131104db
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-14 22:27:33 +0800

    feat(ProductCard): enhance component with product data props and layout

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -1,7 +1,47 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div className="w-full h-55 bg-amber-100 rounded-lg">test</div>;
+interface ProductProps {
+  productData: {
+    image: {
+      thumbnail: string;
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    name: string;
+    category: string;
+    price: number;
+  };
+  // other props
+}
+
+const ProductCard = ({ productData }: ProductProps) => {
+  return (
+    <div className="w-full h-fit  rounded-lg">
+      {/* Product image */}
+      <div className="relative">
+        <img
+          src={productData.image.desktop}
+          className="w-full h-full bg-cover rounded-lg"
+          alt=""
+        />
+        <div className="w-full flex justify-center absolute -bottom-5 left-0 ">
+          <p className="bg-[#212121] text-white py-1 px-3 rounded-full w-fit">
+            Add to cart
+          </p>
+        </div>
+      </div>
+
+      {/* Product Info */}
+      <div className="h-fit py-8">
+        <p>{productData.category}</p>
+        <p className="font-bold">{productData.name}</p>
+        <p className="font-semibold text-[hsl(14,86%,42%)]">
+          ${productData.price.toFixed(2)}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
