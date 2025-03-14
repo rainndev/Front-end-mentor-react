8dc28510a4c1d97137f01d4d765354fcec48fad9
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-14 21:45:08 +0800

    feat(ProductList): add assets for project

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import ProductData from "../data/product-list-data.json";
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className="text-white font-[Red_hat_text]">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        suscipit magnam commodi ad velit rerum non magni in assumenda illo
+        pariatur, mollitia hic exercitationem, officiis repellat, repellendus
+        cum alias quam.
+      </h1>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
