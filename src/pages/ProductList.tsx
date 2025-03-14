98af869e71c0c86b9121524231dafe24bfc101b6
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-14 22:27:38 +0800

    feat(ProductList): update layout and integrate product data into ProductCard

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -3,20 +3,35 @@ import ProductCard from "../component/ProductCard";
 
 const ProductList = () => {
   return (
-    <div className="w-screen h-screen text-[#212121] font-[Red_hat_text] flex justify-center items-center  ">
+    <div className="w-screen min-h-screen bg-[hsl(20,50%,98%)] text-[#212121] font-[Red_hat_text] flex justify-center items-center  ">
       <div className="grid grid-cols-1 md:grid-cols-[65%_35%] w-full  max-w-6xl ">
         {/* Left container */}
-        <div className="bg-amber-300 w-full p-8">
-          <h1 className="text-2xl font-bold">Desserts</h1>
+        <div className="w-full p-8">
+          <h1 className="text-3xl font-bold">Desserts</h1>
           {/* product grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-            {[...Array(9)].map((_, i) => (
-              <ProductCard key={i} />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-5">
+            {ProductData.map((data, i) => (
+              <ProductCard productData={data} key={i} />
             ))}
           </div>
         </div>
         {/* Right container */}
-        <div className="bg-amber-500 w-full">right container</div>
+        <div className="w-full p-8">
+          <div className="bg-white p-4 rounded-2xl">
+            <h1 className="text-[hsl(14,86%,42%)] font-bold text-xl">
+              Your cart (0)
+            </h1>
+            <div className="w-full flex flex-col justify-center items-center p-5">
+              <img
+                src="/Front-end-mentor-react/product-list/images/illustration-empty-cart.svg"
+                alt=""
+              />
+              <p className="text-sm font-semibold text-[hsl(12,20%,44%)]">
+                Your added items will appear here
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
