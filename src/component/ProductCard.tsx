7233e5bb40cba562ac1ea79f9014433917613f86
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-15 22:42:07 +0800

    feat(ProductCard): add quantity control UI to product card component

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 interface ProductProps {
   productData: {
     image: {
@@ -22,11 +22,11 @@ const ProductCard = ({ productData }: ProductProps) => {
       <div className="relative">
         <img
           src={productData.image.desktop}
-          className="w-full h-full bg-cover rounded-lg"
+          className="w-full h-full bg-cover rounded-lg border-2 border-[hsl(14,86%,42%)]"
           alt=""
         />
-        <div className="w-full flex justify-center absolute -bottom-5 left-0 ">
-          <div className="bg-white py-2 px-5 rounded-full w-fit flex  font-medium border border-[hsl(7,20%,60%)]">
+        <div className=" w-full flex justify-center absolute -bottom-5 left-0 ">
+          <div className="relative bg-white py-2 px-5 rounded-full w-fit flex  font-medium border border-[hsl(7,20%,60%)]">
             <img
               src="/Front-end-mentor-react/product-list/images/icon-add-to-cart.svg"
               className="mr-2 w-4"
@@ -35,6 +35,21 @@ const ProductCard = ({ productData }: ProductProps) => {
             <p className="text-xs text-[hsl(14,65%,9%)] font-semibold align-middle">
               Add to cart
             </p>
+
+            {/* Add items - add to cart */}
+            <div className="top-0 left-0 absolute w-full h-full bg-[hsl(14,86%,42%)] flex justify-between items-center rounded-full p-2">
+              <img
+                src="/Front-end-mentor-react/product-list/images/icon-decrement-quantity.svg"
+                className="border-white  w-4 h-4 rounded-full border p-[2px]"
+                alt=""
+              />
+              <p className="text-sm">4</p>
+              <img
+                src="/Front-end-mentor-react/product-list/images/icon-increment-quantity.svg"
+                className=" w-4 h-4 border-white rounded-full border p-[2px]"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
