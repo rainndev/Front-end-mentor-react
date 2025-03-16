424c0a5fe6e8e3c6e6b18210584e68d5ae327c2c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-16 21:59:09 +0800

    feat(ProductCard): replace SVG icons with inline SVGs for increment and decrement quantity buttons

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -52,7 +52,7 @@ const ProductCard = ({ productData, currentId }: ProductProps) => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 onClick={handleAddToCart} // <-- Only show this when quantity is 0
-                className="relative bg-white py-2 px-5 rounded-full w-fit flex font-medium border border-[hsl(7,20%,60%)] "
+                className="relative bg-white py-2 px-5 rounded-full w-fit flex font-medium border border-[hsl(7,20%,60%)] hover:border-[hsl(14,86%,42%)] group"
               >
                 <img
                   src="/Front-end-mentor-react/product-list/images/icon-add-to-cart.svg"
@@ -63,7 +63,7 @@ const ProductCard = ({ productData, currentId }: ProductProps) => {
                   initial={{ x: -50, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   exit={{ x: -50, opacity: 0 }}
-                  className="text-xs text-[hsl(14,65%,9%)] font-semibold align-middle"
+                  className="text-xs text-[hsl(14,65%,9%)] font-semibold align-middle  cursor-pointer group-hover:text-[hsl(14,86%,42%)] "
                 >
                   Add to cart
                 </motion.p>
@@ -79,12 +79,22 @@ const ProductCard = ({ productData, currentId }: ProductProps) => {
                 className="py-2 px-5 top-0 left-0 space-x-7 h-full bg-[hsl(14,86%,42%)] flex justify-between items-center rounded-full font-medium"
               >
                 {/* Decrement quantity */}
-                <img
+                <div
                   onClick={handleRemoveCart}
-                  src="/Front-end-mentor-react/product-list/images/icon-decrement-quantity.svg"
-                  className="border-white  w-4 h-4 rounded-full border p-[2px]"
-                  alt=""
-                />
+                  className="border-white w-4 h-4 p-[2px] rounded-full border cursor-pointer hover:bg-white hover:text-black flex items-center justify-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="2"
+                    fill="currentColor"
+                    viewBox="0 0 10 2"
+                    className="w-full h-full text-white  hover:text-[hsl(14,86%,42%)] transition"
+                  >
+                    <path d="M0 .375h10v1.25H0V.375Z" />
+                  </svg>
+                </div>
+
                 <motion.p
                   key={getQuantity(currentId)}
                   initial={{ y: -10, opacity: 0 }}
@@ -95,12 +105,22 @@ const ProductCard = ({ productData, currentId }: ProductProps) => {
                 </motion.p>
 
                 {/* Increment quantity */}
-                <img
+
+                <div
                   onClick={handleAddToCart}
-                  src="/Front-end-mentor-react/product-list/images/icon-increment-quantity.svg"
-                  className=" w-4 h-4 border-white rounded-full border p-[2px]"
-                  alt=""
-                />
+                  className="border-white w-4 h-4 p-[2px] rounded-full border cursor-pointer hover:bg-white hover:text-black flex items-center justify-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="10"
+                    fill="currentColor"
+                    viewBox="0 0 10 10"
+                    className="w-full h-full text-white  hover:text-[hsl(14,86%,42%)] transition"
+                  >
+                    <path d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" />
+                  </svg>
+                </div>
               </motion.div>
             )}
           </AnimatePresence>
