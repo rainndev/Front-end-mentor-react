da8fbe3dfc026d0f8a78369a547f5ff2603b042e
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-16 21:01:39 +0800

    feat(ItemCart): enhance item display with improved styling and layout adjustments

--- src/component/ItemCart.tsx
+++ src/component/ItemCart.tsx
@@ -9,20 +9,37 @@ const ItemCart = () => {
   }, [cart]);
 
   return (
-    <div className="w-full ">
+    <div className="w-full mt-2">
       {cart.map((data, i) => (
-        <div key={i}>
-          <p>{data.name}</p>
-          <div className="flex space-x-3">
-            <p>{data.quantity}x</p>
-            <p>@{data.price}</p>
-            <p>${data.quantity * data.price}</p>
-            <p
+        <div className="py-4 border-b" key={i}>
+          <p className="text-sm font-semibold text-[hsl(14,65%,9%)]">
+            {data.name}
+          </p>
+          <div className="flex justify-between mt-2">
+            <div className="flex space-x-3 text-sm ">
+              <p className="font-semibold text-[hsl(14,86%,42%)]">
+                {data.quantity}x
+              </p>
+              <p className="font-medium text-[hsl(7,20%,60%)]">
+                @ ${data.price.toFixed(2)}
+              </p>
+              <p className="font-semibold text-[hsl(12,20%,44%)]">
+                ${(data.quantity * data.price).toFixed(2)}
+              </p>
+            </div>
+
+            <img
+              className="rounded-full border-[hsl(7,20%,60%)]  border w-4 h-4 p-[2px]"
+              src="/Front-end-mentor-react/product-list/images/icon-remove-item.svg"
+              alt=""
+            />
+
+            {/* <p
               onClick={() => removeFromCart(data.id)}
               className="ml-4 rounded-full bg-black text-white px-2"
             >
               X
-            </p>
+            </p> */}
           </div>
         </div>
       ))}
