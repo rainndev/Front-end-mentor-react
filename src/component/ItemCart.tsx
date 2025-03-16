7da2381e5ffe8688d6dced9c06629a6b59695d3f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-16 21:10:36 +0800

    feat(ItemCart): enhance item layout with improved structure and styling for better readability

--- src/component/ItemCart.tsx
+++ src/component/ItemCart.tsx
@@ -11,23 +11,34 @@ const ItemCart = () => {
   return (
     <div className="w-full mt-2">
       {cart.map((data, i) => (
-        <div className="py-4 border-b border-[hsl(13,31%,94%)]" key={i}>
-          <p className="text-sm font-semibold text-[hsl(14,65%,9%)]">
-            {data.name}
-          </p>
-          <div className="flex justify-between mt-2">
-            <div className="flex space-x-3 text-sm ">
-              <p className="font-semibold text-[hsl(14,86%,42%)]">
-                {data.quantity}x
-              </p>
-              <p className="font-medium text-[hsl(7,20%,60%)]">
-                @ ${data.price.toFixed(2)}
-              </p>
-              <p className="font-semibold text-[hsl(12,20%,44%)]">
-                ${(data.quantity * data.price).toFixed(2)}
-              </p>
-            </div>
+        <div
+          className="py-4 border-b border-[hsl(13,31%,94%)] flex justify-between"
+          key={i}
+        >
+          <div>
+            {/* Title of product */}
+            <p className="text-sm font-semibold text-[hsl(14,65%,9%)]">
+              {data.name}
+            </p>
 
+            {/* Product Number data */}
+            <div className="flex justify-between mt-2">
+              <div className="flex space-x-3 text-sm ">
+                <p className="font-semibold text-[hsl(14,86%,42%)]">
+                  {data.quantity}x
+                </p>
+                <p className="font-medium text-[hsl(7,20%,60%)]">
+                  @ ${data.price.toFixed(2)}
+                </p>
+                <p className="font-semibold text-[hsl(12,20%,44%)]">
+                  ${(data.quantity * data.price).toFixed(2)}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Remove product button */}
+          <div className="flex justify-center items-center">
             <img
               onClick={() => removeFromCart(data.id)}
               className="rounded-full border-[hsl(7,20%,60%)]  border w-4 h-4 p-[2px] cursor-pointer"
@@ -38,7 +49,10 @@ const ItemCart = () => {
         </div>
       ))}
 
-      <p className="mt-5">Total ${totalPrice}</p>
+      <div className="w-full flex justify-between items-center mt-5">
+        <p className="text-sm">Order Total</p>
+        <p className="font-bold text-xl">${totalPrice}</p>
+      </div>
     </div>
   );
 };
