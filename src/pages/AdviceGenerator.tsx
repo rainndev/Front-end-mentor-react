5b604ba8858d5b6aa2c2ed52aad6915219cd746f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-19 01:17:34 +0800

    feat(AdviceGenerator): add animations for loading state and advice display

--- src/pages/AdviceGenerator.tsx
+++ src/pages/AdviceGenerator.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const AdviceGenerator = () => {
   const [AdviceId, setAdviceId] = useState<string>("");
@@ -25,30 +26,42 @@ const AdviceGenerator = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[hsl(218,23%,16%)] font-Man">
       {/* Container advice */}
-      {loading ? (
-        <p className="text-lg font-bold text-center text-[hsl(193,38%,86%)] drop-shadow-2xl">
-          Loading...
-        </p>
-      ) : (
-        <div className="bg-[hsl(217,19%,24%)] flex justify-center items-center flex-col p-15 space-y-4 max-w-xl rounded-2xl relative">
-          <h1 className="text-[hsl(150,100%,66%)] text-xs font-bold tracking-[.5em]">
-            ADVICE #{AdviceId}
-          </h1>
-          <p className="mt-2 text-2xl font-bold text-center text-[hsl(193,38%,86%)] text-pretty">
-            “{advice}”
-          </p>
-          <img
-            src="/Front-end-mentor-react/advice-generator/images/pattern-divider-desktop.svg"
-            className="mt-5"
-          />
 
-          <div className="absolute -bottom-8 left-0 w-full flex justify-center items-center">
-            <div className=" rounded-full p-5 bg-[hsl(150,100%,66%)]">
-              <img src="/Front-end-mentor-react/advice-generator/images/icon-dice.svg" />
+      <AnimatePresence>
+        {loading ? (
+          <p className="text-lg font-bold text-center text-[hsl(193,38%,86%)] drop-shadow-xl">
+            Loading...
+          </p>
+        ) : (
+          <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 50, opacity: 0 }}
+            className="bg-[hsl(217,19%,24%)] flex justify-center items-center flex-col p-15 space-y-4 max-w-xl rounded-2xl relative"
+          >
+            <h1 className="text-[hsl(150,100%,66%)] text-xs font-bold tracking-[.5em]">
+              ADVICE #{AdviceId}
+            </h1>
+            <p className="mt-2 text-2xl font-bold text-center text-[hsl(193,38%,86%)] text-pretty">
+              “{advice}”
+            </p>
+            <img
+              src="/Front-end-mentor-react/advice-generator/images/pattern-divider-desktop.svg"
+              className="mt-5"
+            />
+
+            <div className="absolute -bottom-8 left-0 w-full flex justify-center items-center">
+              <div className=" rounded-full p-5 bg-[hsl(150,100%,66%)]">
+                <motion.img
+                  initial={{ rotate: 90 }}
+                  animate={{ rotate: 0 }}
+                  src="/Front-end-mentor-react/advice-generator/images/icon-dice.svg"
+                />
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
