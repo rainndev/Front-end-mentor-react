e7ae084b3e5374ffeeead677c5d8981c380408ac
rainndev <182114243+rainndev@users.noreply.github.com>
2025-03-19 00:55:44 +0800

    feat(AdviceGenerator): enhance layout and styling with new font and improved structure

--- src/pages/AdviceGenerator.tsx
+++ src/pages/AdviceGenerator.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const AdviceGenerator = () => {
-  return <div>AdviceGenerator</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-[hsl(218,23%,16%)] font-Man ">
+      <div className="bg-[hsl(217,19%,24%)] flex justify-center items-center flex-col p-15 space-y-5 max-w-xl rounded-2xl relative">
+        <h1 className="text-[hsl(150,100%,66%)] text-xs font-bold tracking-[.5em]">
+          ADVICE #117
+        </h1>
+        <p className="mt-2 text-2xl font-bold text-center text-[hsl(193,38%,86%)] text-pretty">
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </p>
+        <img
+          src="/Front-end-mentor-react/advice-generator/images/pattern-divider-desktop.svg"
+          className="mt-5"
+        />
+
+        <div className="absolute -bottom-8 left-0 w-full flex justify-center items-center">
+          <div className=" rounded-full p-5 bg-[hsl(150,100%,66%)]">
+            <img src="/Front-end-mentor-react/advice-generator/images/icon-dice.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdviceGenerator;
