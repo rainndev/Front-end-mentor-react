6df1dca3ff2e4970f3390392683faebee54ce4da
rainndev <182114243+rainndev@users.noreply.github.com>
2025-09-05 23:09:27 +0800

    feat: add filter buttons

---
+++ src/component/BrowserExtensionManager/FilterButtons.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const filterOptions = ["All", "Active", "Inactive"];
+
+const FilterButtons = () => {
+  const [selectedFilter, setSelectedFilter] = useState(0);
+
+  return (
+    <div className="space-x-2 flex text-BEM-neutral-900">
+      {filterOptions.map((data, i) => (
+        <button
+          onClick={() => setSelectedFilter(i)}
+          className={`p-1 px-4 rounded-full ${
+            selectedFilter === i
+              ? "bg-BEM-red-400 text-BEM-neutral-900"
+              : "bg-BEM-neutral-600 text-BEM-neutral-0"
+          }`}
+        >
+          {data}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FilterButtons;
--- src/pages/BrowserExtensionMain.tsx
+++ src/pages/BrowserExtensionMain.tsx
@@ -1,10 +1,18 @@
+import FilterButtons from "../component/BrowserExtensionManager/FilterButtons";
 import NavigationBar from "../component/BrowserExtensionManager/NavigationBar";
 
 const BrowserExtensionMain = () => {
   return (
     <div className="h-dvh w-full flex justify-center [background-image:var(--color-BEM-dark-gradient)]">
-      <div className="h-dvh flex max-w-7xl w-full">
+      <div className="h-dvh flex max-w-7xl w-full  flex-col">
         <NavigationBar />
+        <div className="flex justify-between mt-10">
+          <h1 className="text-3xl font-medium text-BEM-neutral-100">
+            Extension List
+          </h1>
+
+          <FilterButtons />
+        </div>
       </div>
     </div>
   );
